c0009e0a181e956490b96c591ea55cf8a35e5cbc
Peter <klintsov.p@mail.ru>
2024-05-15 11:49:54 +0300

    toasts added

--- app/src/main/java/com/example/playlist_maker_dev/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker_dev/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.playlist_maker_dev
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.buttonSearch)
+        val mediaButton = findViewById<Button>(R.id.buttonMedia)
+        val settingsButton = findViewById<Button>(R.id.buttonSettings)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Активно ищем!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        searchButton.setOnClickListener(imageClickListener)
+
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Пока треков нет", Toast.LENGTH_SHORT).show()
+        }
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Настройки в разработке", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
+
+
