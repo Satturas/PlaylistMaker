e356d754875745905efcdf937e566bed51fd9417
Peter <klintsov.p@mail.ru>
2024-05-15 12:01:40 +0300

    убраны тосты, добавлены переходы

--- app/src/main/java/com/example/playlist_maker_dev/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker_dev/MainActivity.kt
@@ -1,39 +1,33 @@
 package com.example.playlist_maker_dev
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val searchButton = findViewById<Button>(R.id.buttonSearch)
         val mediaButton = findViewById<Button>(R.id.buttonMedia)
         val settingsButton = findViewById<Button>(R.id.buttonSettings)
 
-        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Активно ищем!", Toast.LENGTH_SHORT).show()
-            }
-
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
         }
-        searchButton.setOnClickListener(imageClickListener)
 
         mediaButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Пока треков нет", Toast.LENGTH_SHORT).show()
+            val displayIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayIntent)
         }
 
         settingsButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Настройки в разработке", Toast.LENGTH_SHORT)
-                .show()
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
     }
 }
-
-
