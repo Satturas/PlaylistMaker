ebd04dd5e1d988f264b53b072dd37fe6476c70c8
Peter <klintsov.p@mail.ru>
2024-05-21 09:52:30 +0300

    реализована кнопка write to support

--- app/src/main/java/com/example/playlist_maker_dev/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker_dev/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlist_maker_dev
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,10 +13,28 @@ class SettingsActivity : AppCompatActivity() {
             putExtra(Intent.EXTRA_TEXT, this@shareTextToOtherApps)
             type = "text/plain"
         }
-
         val shareIntent = Intent.createChooser(sendIntent, null)
         startActivity(shareIntent)
     }
+
+    private fun writeToSupport() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, "klintsov.p@mail.ru")
+            putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -28,6 +47,11 @@ class SettingsActivity : AppCompatActivity() {
         val shareButton = findViewById<Button>(R.id.buttonShareApp)
         shareButton.setOnClickListener {
             "https://practicum.yandex.ru/android-developer/".shareTextToOtherApps()
+        }
+
+        val supportButton = findViewById<Button>(R.id.buttonWriteToSupport)
+        supportButton.setOnClickListener {
+            writeToSupport()
         }
     }
 }
