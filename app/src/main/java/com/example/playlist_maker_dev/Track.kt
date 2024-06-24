227196896a8dcbc9c1dde9ee8c2b14d9749f4911
Peter <klintsov.p@mail.ru>
2024-06-24 10:46:56 +0300

    added response and api

--- app/src/main/java/com/example/playlist_maker_dev/Track.kt
+++ app/src/main/java/com/example/playlist_maker_dev/Track.kt
@@ -1,8 +1,21 @@
 package com.example.playlist_maker_dev
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 data class Track (
     val trackName: String,
     val artistName: String,
     val trackTime: String,
     val artworkUrl100: String?) {
+}
+
+class TrackResponse(val searchType: String,
+                     val expression: String,
+                     val results: List<Track>)
+
+interface ITunesApi {
+    @GET("/search?entity=song")
+    fun findTrack(@Query("term") text: String): Call<TrackResponse>
 }
