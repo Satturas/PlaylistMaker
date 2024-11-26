2d56aa6784136008f668517be74f0d631d17a64b
Peter <klintsov.p@mail.ru>
2024-11-26 12:14:40 +0300

    Room: работа над ui слоем медиатеки

--- app/src/main/java/com/example/playlist_maker_dev/media/ui/MediaViewModel.kt
+++ app/src/main/java/com/example/playlist_maker_dev/media/ui/MediaViewModel.kt
@@ -1,39 +1,6 @@
 package com.example.playlist_maker_dev.media.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.playlist_maker_dev.media.domain.db.FavouritesInteractor
-import com.example.playlist_maker_dev.search.domain.models.Track
-import kotlinx.coroutines.launch
 
-class MediaViewModel(
-    private val favouritesInteractor: FavouritesInteractor
-) : ViewModel() {
-
-    private val _mediaState = MutableLiveData<MediaState>()
-    val mediaState: LiveData<MediaState> = _mediaState
-
-    init {
-        fun fillData() {
-            viewModelScope.launch {
-                favouritesInteractor
-                    .getFavouriteTracks()
-                    .collect { tracks -> processResult(tracks) }
-            }
-        }
-    }
-
-    private fun processResult(tracks: List<Track>) {
-        if (tracks.isEmpty()) {
-            renderState(MediaState.NothingInFavourite)
-        } else {
-            renderState(MediaState.FavouriteTracks(tracks))
-        }
-    }
-
-    private fun renderState(state: MediaState) {
-        _mediaState.postValue(state)
-    }
+class MediaViewModel() : ViewModel() {
 }
