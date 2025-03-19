37f39cda0154182178ea90eebcf1bc465ce76ecb
Peter <klintsov.p@mail.ru>
2025-03-19 12:21:41 +0300

    спринт 27: начало верстки экрана поиска

--- app/src/main/java/com/example/playlist_maker_dev/search/ui/SearchScreen.kt
+++ app/src/main/java/com/example/playlist_maker_dev/search/ui/SearchScreen.kt
@@ -1,2 +1,82 @@
 package com.example.playlist_maker_dev.search.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.playlist_maker_dev.R
+
+@Composable
+fun SearchScreen() {
+    MyScaffold()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyScaffold() {
+
+    val colors =
+        listOf(
+            Color(0xFFffd7d7.toInt()),
+            Color(0xFFffe9d6.toInt()),
+            Color(0xFFfffbd0.toInt()),
+            Color(0xFFe3ffd9.toInt()),
+            Color(0xFFd0fff8.toInt())
+        )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.search_text)) },
+                /*navigationIcon = {
+                    IconButton(onClick = { /* "Open nav drawer" */ }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }*/
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                // consume insets as scaffold doesn't do it by default
+                modifier = Modifier.consumeWindowInsets(innerPadding),
+                contentPadding = innerPadding
+            ) {
+                items(count = 100) {
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                            .background(colors[it % colors.size]))
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun SearhScreenPreview(){
+    SearchScreen()
+}
